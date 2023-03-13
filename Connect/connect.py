c55603361866c862b02b68fc0d68451e3a904c82
samriddhsingh123 <samridhsingh2001@gmail.com>
2023-03-13 19:47:23 +0530

    Query added

--- Connect/connect.py
+++ Connect/connect.py
@@ -1,4 +1,5 @@
 import mysql.connector
+
 conn = mysql.connector.connect(host='localhost', password='password', user='root', database = 'ir_policy_db')
 
 if(conn.is_connected()):
@@ -9,7 +10,13 @@ else:
 mycursor = conn.cursor()
 
 mycursor.execute("select * from ir_policy_db.Apps_table join ir_policy_db.app_permission join ir_policy_db.Permission where ir_policy_db.Apps_table.App_Id = ir_policy_db.app_permission.appID and ir_policy_db.app_permission.permissionID = ir_policy_db.Permission.PermissionID;")
+
 result = mycursor.fetchall()
+mycrsr=conn.cursor()
+mycrsr.execute("select ir_policy_db.Apps_table.App_Id, ir_policy_db.Apps_table.App_Name, ir_policy_db.Apps_table.Privacy_policy, ir_policy_db.Apps_table.Summary, ir_policy_db.Permission.Name as Access, ir_policy_db.Permission.Description from ir_policy_db.Apps_table join ir_policy_db.app_permission join ir_policy_db.Permission where ir_policy_db.Apps_table.App_Id = ir_policy_db.app_permission.appID and ir_policy_db.app_permission.permissionID = ir_policy_db.Permission.PermissionID")
+res=mycrsr.fetchall()
+
 print(result[0])
+print(res[0])
 
 
