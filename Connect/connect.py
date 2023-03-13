e549d1bfbd236e44015b361078722e1ab613ffc8
samriddhsingh123 <samridhsingh2001@gmail.com>
2023-03-13 19:54:51 +0530

    query added and code revised

--- Connect/connect.py
+++ Connect/connect.py
@@ -16,6 +16,12 @@ mycrsr=conn.cursor()
 mycrsr.execute("select ir_policy_db.Apps_table.App_Id, ir_policy_db.Apps_table.App_Name, ir_policy_db.Apps_table.Privacy_policy, ir_policy_db.Apps_table.Summary, ir_policy_db.Permission.Name as Access, ir_policy_db.Permission.Description from ir_policy_db.Apps_table join ir_policy_db.app_permission join ir_policy_db.Permission where ir_policy_db.Apps_table.App_Id = ir_policy_db.app_permission.appID and ir_policy_db.app_permission.permissionID = ir_policy_db.Permission.PermissionID")
 res=mycrsr.fetchall()
 
+
+crsr=conn.cursor()
+crsr.execute("select ir_policy_db.Apps_table.App_Id, ir_policy_db.Apps_table.App_Name, ir_policy_db.Apps_table.Privacy_policy, ir_policy_db.Apps_table.Summary, ir_policy_db.feature.description as Feature from ir_policy_db.Apps_table join ir_policy_db.app_features join ir_policy_db.feature where ir_policy_db.Apps_table.App_Id = ir_policy_db.app_features.appID and ir_policy_db.app_features.featureID = ir_policy_db.feature.featureID;")
+res3=crsr.fetchall()
+
+print(res3[0])
 print(result[0])
 print(res[0])
 
